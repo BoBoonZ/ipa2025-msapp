31c373f11c494bb358aa80e0121c7eb726a2da8b
BoBoonZ <66070108@kmitl.ac.th>
2025-09-02 16:24:31 +0700

    pep8

--- scheduler/producer.py
+++ scheduler/producer.py
@@ -5,7 +5,8 @@ rabbitmq_host = os.getenv("RABBITMQ_HOST", "rabbitmq")
 rabbitmq_user = os.getenv("RABBITMQ_USER", "admin")
 rabbitmq_pass = os.getenv("RABBITMQ_PASS", "rabbitmq")
 
-def  produce(host, body):
+
+def produce(host, body):
     credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_pass)
 
     connection = pika.BlockingConnection(
@@ -19,14 +20,14 @@ def  produce(host, body):
 
     channel.exchange_declare(exchange="jobs", exchange_type="direct")
     channel.queue_declare(queue="router_jobs")
-    channel.queue_bind(queue="router_jobs", exchange="jobs",
-    routing_key="check_interfaces")
+    channel.queue_bind(queue="router_jobs",
+    exchange="jobs", routing_key="check_interfaces")
 
     channel.basic_publish(exchange="jobs",
     routing_key="check_interfaces", body=body)
 
     connection.close()
 
+
 if __name__ == "__main__":
     produce("localhost", "192.168.1.44")
-
